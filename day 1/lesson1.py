8a0a77ee97b35d06cf336b23cdff01181551de3a
Saba123456780 <nakashidzesaba41@gmail.com>
2024-02-04 22:02:28 +0400

    2th commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,37 @@
+# name = "giorgi"
+# print(name)
+
+
+
+
+
+# name = "gio"       #   ყველაფერი რაც იქნება მოქცეული " " ბრჭყალებში ეწოდება str - string
+# age = 15      # ყველა რიცხვს ეწოდება int - integer
+# print(age)
+
+
+
+
+# num = '5' + "1"
+# print(num)
+
+
+# sport = "basket" + "ball"
+# print(sport)
+
+
+
+
+# name = "gio"      
+# age = 15
+# print(name + str(age) )
+
+
+
+
+
+
+name = "saba"
+lastname = "nakashidze"
+age = "14"
+print("me mqvia " + name + " " + lastname + " da var " + age + " wlis")
