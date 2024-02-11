245d5ed5886547efa6788dffe851d3dba975482f
Saba123456780 <nakashidzesaba41@gmail.com>
2024-02-11 19:52:52 +0400

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,11 @@
+
+
+name = "saba"
+lastname = "Nakashidze"
+age = "14"
+height = "1.72"
+petname = "haven't pet" 
+gmail = "nakashidzesaba41@gmail.com"
+number = "571 02 11 55"
+adress = "didi digomi IV M/R CORPS VI Hallway VIII floor VIII"
+print("me mqvia " + name + "  " + lastname + " da var " + age + " wlis " + "  "  + " var " + height + " " +" chemi gmail aris " +  gmail + " chemi nomeria"  " " + number + "  " " misamartia "+ adress )
---
+++ day 2/lesson2.py
@@ -0,0 +1,8 @@
+
+
+print("gavicani da davumegobrdi" + "  "+  "gio gagnidzes")
+
+
+print("Goa aris skola sadac shevdzlebt sheviswavlot yvelaferi aris sauketeso akademia aseve yavs sauketese maswabvlebeli, romelic yvelafers aketebs rom chven  viswavloyt  ")
+
+
