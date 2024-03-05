21a170c6dd931a76d9da3b5b8233f1684c924a51
Saba123456780 <nakashidzesaba41@gmail.com>
2024-03-05 14:49:49 +0400

    second commit

---
+++ day 04/homework/lesson4.py
@@ -0,0 +1,10 @@
+
+
+#3) ჰკითხეთ მომხმარებელს 1) სახელი 2) რამდენი წლისაა, 3) მომავალში რამდენი წლის შემდეგ უნდა დროში მოგზაურობა.
+
+user_name = str(input(" whats your name : "))
+user_age = str(input(" what is your age : "))
+user_travel = str(input(" what year would you travel : "))
+
+print(" your name is  " + user_name + "  your age is " + user_age + " you would to travel " + user_travel)
+
---
+++ day 04/homework1/lesson04.py
@@ -0,0 +1,9 @@
+
+# 2) შექმენით პროგრამა სადაც შეეძლებათ მომხმარებელს შემოიტანოს 2 რიცხვი input() ფუნქციის მეშვეობით და საბოლოოდ გამოიტანეთ ორი რიცხვის ჯამი, მაგ 5 + 5 = 10.
+
+num1 = int(input(" please enter first number: "))
+num2 = int(input(" please enter second number: "))
+
+print(num1 + num2)
+
+
