162bb6d75eb30d10e2000aad33dad6c6905bde9b
Saba123456780 <nakashidzesaba41@gmail.com>
2024-03-18 20:32:01 +0400

    second commit

---
+++ day 07/homework7.py/lesson7.py
@@ -0,0 +1,7 @@
+
+#მომხმარებელს შემოატანინეთ მართკუთხედის (rectangle) გვერდები (გაითვალისწინეთ, რომ შესაძლოა რიცხვი ათწილადიც იყოს) და დაუბეჭდეთ მისი ფართობი.
+
+user_page = float(input("please enter rectangle page :")) 
+
+print(user_page +  user_page * 2)
+
---
+++ day 07/homework7.py/lessoon07.py
@@ -0,0 +1,8 @@
+
+
+#მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ არის თუ არა ის 100-ზე მეტი და 9-ის ჯერადი (9-ზე სრულად იყოფა).
+
+
+user_number = int(input("please enter number :"))
+
+print(user_number > 100 and user_number / 9)
